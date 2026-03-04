05e828fcc6621afca257662f367186e9161fcfff
Valeria Sanguinetti <valeriasanguinetti07@gmail.com>
2026-03-04 20:57:19 -0300

    actualizado

--- js/auth.js
+++ js/auth.js
@@ -13,43 +13,31 @@ const btnLogin = document.getElementById("btnLogin");
 const btnRegister = document.getElementById("btnRegister");
 
 if (btnRegister) {
-  btnRegister.addEventListener("click", async () => {
-    mensaje.textContent = "";
-    const email = emailInput.value;
-    const password = passwordInput.value;
+ btnRegister.addEventListener("click", async () => {
+  mensaje.textContent = "";
+  const email = emailInput.value;
+  const password = passwordInput.value;
 
-    try {
-      const { data, error } = await window.supabaseClient.auth.signUp({
-        email,
-        password,
-      });
+  try {
+    const { data, error } = await window.supabaseClient.auth.signUp({
+      email,
+      password,
+    });
 
-      // 🔹 Logs para depuración
-    //  console.log("Registro:", { data, error });
-
-      if (error) {
-        mensaje.textContent = "⚠️ " + error.message;
-        mensaje.style.color = "red";
-        return;
-      }
-
-      // Revisar si se creó un usuario o no
-    if (!data.session && data.user && data.user.email === email) {
-      mensaje.textContent = "⚠️ Este email ya está registrado";
+    if (error) {
+      mensaje.textContent = "⚠️ " + error.message;
       mensaje.style.color = "red";
       return;
     }
-      // Usuario creado correctamente o pendiente confirmación
-      //console.log("Usuario registrado correctamente", data);
-      mensaje.textContent = "✅ Registro exitoso (verifica tu email)";
-      mensaje.style.color = "green";
 
-    } catch (err) {
-      //console.error("Error en el registro:", err);
-      mensaje.textContent = "⚠️ Ocurrió un error inesperado";
-      mensaje.style.color = "red";
-    }
-  });
+    mensaje.textContent = "✅ Revisa tu email para confirmar tu cuenta";
+    mensaje.style.color = "green";
+
+  } catch (err) {
+    mensaje.textContent = "⚠️ Ocurrió un error inesperado";
+    mensaje.style.color = "red";
+  }
+});
 }
 if (btnLogin) {
   btnLogin.addEventListener("click", async () => {
